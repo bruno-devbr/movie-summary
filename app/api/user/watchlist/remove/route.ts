9946951319f3e9c9929123a38d6f90a441d71dc4
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-12-15 13:33:27 -0300

    feat: route api for user watchlist

---
+++ app/api/user/watchlist/remove/route.ts
@@ -0,0 +1,31 @@
+import { api, getError } from "@/app/utils/api";
+import { ActionListBody } from "@/app/utils/types/lists";
+import { NextRequest, NextResponse } from "next/server";
+
+// POST de remover da watchlist do usuario
+export async function POST(request: NextRequest) {
+    try {
+        // pega os cookies e body do request
+        const cookies = await request.cookies;
+        const body = (await request.json()) as ActionListBody;
+
+        // pega o account_id e session_id dos cookies
+        const account_id = cookies.get("account_id")?.value;
+        const session_id = cookies.get("session_id")?.value || "";
+
+        // faz o post enviando o body
+        await api.post(
+            `/account/${account_id}/watchlist?session_id=${session_id}`,
+            body
+        );
+
+        // retorna status 200
+        return NextResponse.json(
+            { message: "removido da watchlist com sucesso" },
+            { status: 200 }
+        );
+    } catch (error) {
+        // função de tratamento de erros
+        return getError(error);
+    }
+}
