a076f670262ad3eb409f33063b140f6b840c5f2a
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-12-15 16:39:00 -0300

    feat: route api for multiSearch + movies

--- app/api/movies/[movie_id]/route.ts
+++ app/api/movies/[movie_id]/route.ts
@@ -5,10 +5,13 @@ type ParamsProps = {
     params: { movie_id: string };
 };
 
+// GET de movie pelo id
 export async function GET(request: NextRequest, { params }: ParamsProps) {
     try {
-        const { movie_id } = await params;
+        // pega o movie_id do params
+        const { movie_id } = params;
 
+        // faz o get retornando um Movie
         const res = await api.get<Movie>(`/movie/${movie_id}`, {
             params: {
                 append_to_response: "credits,reviews,similar,videos",
@@ -16,8 +19,10 @@ export async function GET(request: NextRequest, { params }: ParamsProps) {
             },
         });
 
+        // retorna status 200
         return NextResponse.json(res.data, { status: 200 });
     } catch (error) {
+        // função de tratamento de erros
         return getError(error);
     }
 }
