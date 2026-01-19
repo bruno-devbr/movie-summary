7177303761810c1d056f49eff662c211039bdcc3
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-01-19 18:57:13 -0300

    fix: update user dropdown contents to use icon components directly

--- app/utils/header/userDropdownContents.tsx
+++ app/utils/header/userDropdownContents.tsx
@@ -2,7 +2,7 @@ import { Bookmark, Heart, List } from "lucide-react";
 import { UserDropDownProps } from "../types/UserDropdown";
 
 export const userContents: UserDropDownProps[] = [
-    { link: "/user/listas", icon: <List />, text: "Minhas Listas" },
-    { link: "/user/watchlist", icon: <Bookmark />, text: "WatchList" },
-    { link: "/user/favoritos", icon: <Heart />, text: "Favoritos" },
+    { link: "/user/listas", icon: List, text: "Minhas Listas" },
+    { link: "/user/watchlist", icon: Bookmark, text: "WatchList" },
+    { link: "/user/favoritos", icon: Heart, text: "Favoritos" },
 ];
