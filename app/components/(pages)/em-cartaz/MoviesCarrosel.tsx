9ee20be5d82623e5c5b655d2294f5030d838dcf3
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-01-27 17:08:37 -0300

    feat: implement timer component for movie carousel with pause functionality

--- app/components/(pages)/em-cartaz/MoviesCarrosel.tsx
+++ app/components/(pages)/em-cartaz/MoviesCarrosel.tsx
@@ -1,18 +1,19 @@
 import { MoviesList } from "@/app/utils/types/moviesSchema";
 import { Background } from "./Background";
 import { Navigation } from "./Navigation";
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { Timer } from "./Timer";
 
 export function MoviesCarrosel({ movies }: { movies: MoviesList }) {
-    const [index, setIndex] = useState(0);
+    const [isAwaysPaused, setIsAwaysPaused] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
 
-    const handleNext = () => {
-        if (index === movies.results.length - 1) {
-            setIndex(0);
-        } else {
-            setIndex((prev) => prev + 1);
-        }
-    };
+    const [index, setIndex] = useState(0);
+    const [count, setCount] = useState(5);
+
+    const handleNext = useCallback(() => {
+        setIndex((prev) => (prev === movies.results.length - 1 ? 0 : prev + 1));
+    }, [movies.results.length]);
 
     const handlePrev = () => {
         if (index === 0) {
@@ -22,10 +23,53 @@ export function MoviesCarrosel({ movies }: { movies: MoviesList }) {
         }
     };
 
+    function handleMouseEnter() {
+        if (!isAwaysPaused) {
+            setIsPaused(true);
+        }
+    }
+
+    function handleMouseLeave() {
+        if (!isAwaysPaused) {
+            setIsPaused(false);
+        }
+    }
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout | undefined;
+
+        if (!isPaused) {
+            interval = setInterval(() => {
+                setCount((prev) => {
+                    if (prev === 0) {
+                        handleNext();
+                        return 5;
+                    }
+
+                    return prev - 1;
+                });
+            }, 1000);
+        }
+
+        return () => {
+            if (interval) clearInterval(interval);
+        };
+    }, [isPaused, handleNext]);
+
     return (
-        <div className="relative h-125 w-full overflow-hidden">
+        <div
+            className="relative h-125 w-full overflow-hidden"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
             <Background movie={movies.results[index]} />
             <Navigation handleNext={handleNext} handlePrev={handlePrev} />
+            <Timer
+                count={count}
+                isPaused={isPaused}
+                setIsAwaysPaused={setIsAwaysPaused}
+                IsAwaysPaused={isAwaysPaused}
+            />
         </div>
     );
 }
