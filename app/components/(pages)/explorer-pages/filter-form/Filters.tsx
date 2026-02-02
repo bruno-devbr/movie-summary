3a5e91246bd17085fafe8e4a38a596ef0190561d
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-02-01 22:06:38 -0300

    feat: enhance Filters and MiddleWrapper components with improved rendering and genre selection functionality

--- app/components/(pages)/explorer-pages/filter-form/Filters.tsx
+++ app/components/(pages)/explorer-pages/filter-form/Filters.tsx
@@ -6,14 +6,12 @@ export function Filters() {
     const { isOpen } = useFilters();
 
     return (
-        <>
-            {isOpen && (
-                <form className="bg-gray-800 rounded-lg p-6 mb-8">
-                    <h3 className="text-xl mb-4">Filtros</h3>
-                    <TopWrapper />
-                    <MiddleWrapper />
-                </form>
-            )}
-        </>
+        <form
+            className={`bg-gray-800 rounded-lg p-6 mb-8 transition-all duration-200 ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+        >
+            <h3 className="text-xl mb-4">Filtros</h3>
+            <TopWrapper />
+            <MiddleWrapper />
+        </form>
     );
 }
--- app/components/(pages)/explorer-pages/filter-form/MidleWrapper.tsx
+++ app/components/(pages)/explorer-pages/filter-form/MidleWrapper.tsx
@@ -1,11 +1,13 @@
-import { useGlobalStates } from "@/app/utils/hooks/store";
-import { GenresSchema } from "@/app/utils/types/genres";
+import { useFilters, useGlobalStates } from "@/app/utils/hooks/store";
+import { GenresList, GenresSchema } from "@/app/utils/types/genres";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export function MiddleWrapper() {
-    const { setError } = useGlobalStates();
     const [data, setData] = useState<GenresList | null>(null);
+
+    const { setError } = useGlobalStates();
+    const { setGenres, genres } = useFilters();
 
     useEffect(() => {
         if (data) return;
@@ -26,10 +28,34 @@ export function MiddleWrapper() {
         loadData();
     }, [setData, setError, data]);
 
+    const handleClick = (id: number, isIncluding: boolean) => {
+        if (isIncluding) {
+            setGenres(genres?.filter((g) => g !== id));
+        } else {
+            setGenres([...genres, id]);
+        }
+    };
+
+    if (!data) return null;
     return (
         <div className="mt-6">
             <label className="block mb-2 text-sm text-gray-400">Gêneros</label>
-            <div className="flex flex-wrap gap-2"></div>
+            <div className="flex flex-wrap gap-2">
+                {data.map((genre) => {
+                    const isIncluding = genres?.includes(genre.id);
+
+                    return (
+                        <button
+                            key={genre.id}
+                            type="button"
+                            onClick={() => handleClick(genre.id, isIncluding)}
+                            className={`px-3 py-1 rounded-full text-sm transition-colors text-gray-300 ${isIncluding ? "bg-blue-600 hover:bg-blue-700" : "bg-gray-700 hover:bg-gray-600"} `}
+                        >
+                            {genre.name}
+                        </button>
+                    );
+                })}
+            </div>
         </div>
     );
 }
