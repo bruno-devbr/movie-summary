3629d8fd606f57a8ceb365c9f107afa01231365a
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-02-21 08:42:01 -0300

    adiciona rotas GET para filmes em cartaz, populares, mais bem avaliados e próximas estreias

---
+++ app/api/movies/now_playing/route.ts
@@ -0,0 +1,23 @@
+import { getApi, getError } from "@/app/utils/api/api";
+import { searchParams } from "@/app/utils/api/searchParams";
+import { NextRequest, NextResponse } from "next/server";
+
+// GET de filmes em cartaz
+export async function GET(req: NextRequest) {
+    try {
+        const api = getApi(req); // cria o obj api
+        const { page } = searchParams(req); // pega o page do searchParams
+
+        // faz o fetch passando page
+        const rawData = await api.movieNowPlaying({
+            language: "pt-BR",
+            region: "BR",
+            page,
+        });
+
+        // retorna rawData com status 200
+        return NextResponse.json(rawData, { status: 200 });
+    } catch (error) {
+        return getError(error);
+    }
+}
