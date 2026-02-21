951e9dc44b69235e88218fd3e74381cf22c2abed
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-02-21 11:29:56 -0300

    adiciona rota GET para obter a lista de gêneros de séries

---
+++ app/api/tv/genres/route.ts
@@ -0,0 +1,19 @@
+import { getApi, getError } from "@/app/utils/api/api";
+import { NextRequest, NextResponse } from "next/server";
+
+// GET que retorna a lista de gêneros de series
+export async function GET(req: NextRequest) {
+    try {
+        const api = getApi(req); // cria o obj api
+
+        // faz fetch para obter a lista de gêneros de series
+        const rawData = await api.genreTvList({
+            language: "pt-BR",
+        });
+
+        // retorna o rawData com status 200
+        return NextResponse.json(rawData, { status: 200 });
+    } catch (error) {
+        return getError(error); // trata erros
+    }
+}
