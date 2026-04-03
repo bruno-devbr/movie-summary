65bf6e0af3b7b975a8e08c2036a001ec75f46477
bruno-devbr <brunoqueiroz0707@gmail.com>
2026-04-03 18:28:43 -0300

    feat: add route api for user watchlist details

--- app/api/user/watchlist/route.ts
+++ app/api/user/watchlist/route.ts
@@ -3,16 +3,16 @@ import { getCookies } from "@/app/utils/api/getCookies";
 import { getSearchParams } from "@/app/utils/api/getSearchParams";
 import { NextRequest, NextResponse } from "next/server";
 
-// GET que retorna os dados do usuario
+// GET que retorna a watchlist do usuario
 export async function GET(req: NextRequest) {
     try {
-        const api = getApi(req); // cria obj api
+        const api = getApi(req); // cria o obj api
 
         const { account_id } = getCookies(req); // pega o account_id dos cookies
-        const { page } = getSearchParams(req); // pega o page do search params
+        const { page } = getSearchParams(req); // pega o page do search Params
 
-        // cria o rawData
-        const rawData = await api.accountFavoriteMovies({
+        // faz o fetch criando o rawData
+        const rawData = await api.accountMovieWatchlist({
             language: "pt-BR",
             id: account_id,
             page,
@@ -21,6 +21,6 @@ export async function GET(req: NextRequest) {
         // retorna o rawData com status 200
         return NextResponse.json(rawData);
     } catch (error) {
-        return getError(error); // retorna erro personalizado
+        return getError(error);
     }
 }
